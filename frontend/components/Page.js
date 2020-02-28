380c76e10d12658c2d7413953945dabac7c8ce59
Brian Eyster <brian@bluegrass.com>
2020-02-28 01:34:20 -0700

    Temporarily add styled component example code
    Playing around with styled components, so I added a temporary
    MyButton class via styled.button.

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palegoldenrod;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-size: ${props => (props.huge ? props.huge + 'px' : '1px')};
+  .poop {
+    font-size: 10px;
+  }
+`;
 
 class Page extends React.Component {
   render() {
@@ -8,6 +21,10 @@ class Page extends React.Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge="10">
+          Click me!
+          <span className="poop">💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     );
