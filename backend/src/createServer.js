687365cc57c4bb6ef6f065123579416903f8e5ae
Brian Eyster <brian@bluegrass.com>
2020-03-15 00:50:32 -0600

    Add initial code to create graphql-yoga server

---
+++ backend/src/createServer.js
@@ -0,0 +1,24 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+
+const db = require('./db');
+
+// Create the GraphQL Yoga server
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: 'src/schema.graphql',
+    resolvers: {
+      Mutation: Mutation,
+      Query: Query,
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false,
+    },
+    context: req => ({ ...req, db }),
+  });
+}
+
+module.exports = createServer;
