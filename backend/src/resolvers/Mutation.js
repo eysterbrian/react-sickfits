77f529a52ecbecdfd1c37888efc19db12e18b7c3
Brian Eyster <eysterbrian@gmail.com>
2020-06-09 12:18:38 -0600

    Adds initial query and mutation for tmp Dog class
    - For learning purposes, we defined a simple Dog class with simple
    query and mutation operations.
    - To simplify things (and not have to use Prisma/DB backend), we're just
     using node's global object to hold the dogs array.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutation = {};
+const Mutation = {
+  createDog: function(parent, args, ctx, info) {
+    console.log(args);
+    if (!global.dogs) global.dogs = [];
+
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  },
+};
 
 module.exports = Mutation;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info) {
+    // return [{ name: 'Georgie' }, { name: 'Buddy' }];
+    if (!global.dogs) global.dogs = [];
+    return global.dogs;
+  },
+};
 
 module.exports = Query;
