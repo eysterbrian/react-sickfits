2c72d693bce0c26e1ee00e615c95487a26ba03b2
Brian Eyster <eysterbrian@gmail.com>
2020-06-19 10:32:55 -0600

    Adds page for single item
    Pretty straightforward:
    - Add 1 new page component/route for 'item'
    - Add 1 new component for SingleItem
    - SingleItem has a graphql query w 1 variable
    - We use Query component
    - Use styled-component for the css

---
+++ frontend/components/SingleItem.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import styled from 'styled-components';
+import Head from 'next/head';
+import ErrorMessages from './ErrorMessage';
+
+const SingleItemStyles = styled.div`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${(props) => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
+`;
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      image
+      largeImage
+      priceCents
+    }
+  }
+`;
+
+class SingleItem extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+  };
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <ErrorMessages error={error} />;
+          if (!data.item) return <p>No item found for ID {this.props.id}</p>;
+          const item = data.item;
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Brian Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyles>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default SingleItem;
