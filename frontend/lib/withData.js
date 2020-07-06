e7a8e49590a5ebfabe86ccd68ce2d141750c7081
Brian Eyster <eysterbrian@gmail.com>
2020-07-06 16:10:41 -0600

    Use Apollo local state to open & close the cart
    - adds clientState block to the ApolloClient constructor in withData
    - adds the toggleCart mutation to the clientState resolver map. This
    resolver gets
    - Inside Cart, we wrap the cart code in a Query and Mutation to get/set
    the current cartOpen state.
    - wrap a new 'My Cart' button in the Nav component with the togglecart
    mutation

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,7 @@
 import nextWithApollo from 'next-with-apollo'; // provides HOC to expose apollo within Next
 import ApolloClient from 'apollo-boost'; // sensible defaults for apollo client
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -15,6 +16,26 @@ function createClient({ headers }) {
         },
         headers,
       });
+    },
+
+    // Define our local store using the apollo-boost API (instead of apollo-link-state)
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_root, args, ctx, _info) {
+            const cache = ctx.cache;
+            // Get the current state of cartOpen from local state
+            const { cartOpen } = cache.readQuery({ query: LOCAL_STATE_QUERY });
+            // Toggle current value. We'll return this toggled value.
+            const data = { data: { cartOpen: !cartOpen } };
+            cache.writeData(data);
+            return data;
+          },
+        },
+      },
+      defaults: {
+        cartOpen: false,
+      },
     },
   });
 }
