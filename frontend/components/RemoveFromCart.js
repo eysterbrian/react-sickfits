3892c7dd8ea4300ef650986eb6f3101c65cd92e7
Brian Eyster <eysterbrian@gmail.com>
2020-07-07 16:55:47 -0600

    Updates cache after RemoveFromCart
    - Uses the Mutation's update prop to manually update the local cache
    by removing the CartItem

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -23,21 +23,40 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
-const RemoveFromCart = ({ itemId }) => (
-  <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ itemId }}>
-    {(removeFromCartMutation, { loading, data }) => (
-      <RemoveFromCartStyles
-        disabled={loading}
-        aria-busy={loading}
-        onClick={() =>
-          removeFromCartMutation().catch((err) => alert(err.message))
-        }
-      >
-        &times;
-      </RemoveFromCartStyles>
-    )}
-  </Mutation>
-);
+const RemoveFromCart = ({ itemId }) => {
+  const updateCache = (cache, payload) => {
+    // Get the current cart from the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+
+    // Remove the deleted item from this cart
+    data.me.cart = data.me.cart.filter(
+      (item) => item.id !== payload.data.removeFromCart.id
+    );
+
+    // Update the cache with the cart
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+
+  return (
+    <Mutation
+      mutation={REMOVE_FROM_CART_MUTATION}
+      variables={{ itemId }}
+      update={updateCache}
+    >
+      {(removeFromCartMutation, { loading, data }) => (
+        <RemoveFromCartStyles
+          disabled={loading}
+          aria-busy={loading}
+          onClick={() =>
+            removeFromCartMutation().catch((err) => alert(err.message))
+          }
+        >
+          &times;
+        </RemoveFromCartStyles>
+      )}
+    </Mutation>
+  );
+};
 
 RemoveFromCart.propTypes = {
   itemId: PropTypes.string.isRequired,
