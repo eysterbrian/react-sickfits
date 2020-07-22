aeca24d8968b6d2cff23e53dfcc3210edbffab2b
Brian Eyster <eysterbrian@gmail.com>
2020-07-22 12:26:46 -0600

    Invoke StripeCheckout from checkout button
    - Adds new TakeMyMoney component uses StripeCheckout component
    to invoke the client-side Stripe checkout GUI
    - In Cart we wrap the Checkout button in our new TakeMyMoney
    component.
    - TODO: Currently our onToken prop just logs the Stripe token, so we
    never charge this token

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -10,6 +10,7 @@ import User from './User';
 import CartItem from './CartItem';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import TakeMyMoney from './TakeMyMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query LOCAL_STATE_QUERY {
@@ -23,9 +24,10 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
-// Each of our components uses children as the render prop, so to avoid proptype errors
-// we explicitly pass the render as the children prop
+// Use react-adopt to compose several queries/mutations at once
 const Composed = adopt({
+  // Each of our components uses children as the render prop, so to avoid proptype errors
+  // we explicitly pass the render as the children prop
   user: ({ render }) => <User>{render}</User>,
   toggleCart: ({ render }) => (
     <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
@@ -58,7 +60,9 @@ const Cart = (props) => {
             </ul>
             <footer>
               <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-              <SickButton>Checkout</SickButton>
+              <TakeMyMoney>
+                <SickButton>Checkout</SickButton>
+              </TakeMyMoney>
             </footer>
           </CartStyles>
         );
